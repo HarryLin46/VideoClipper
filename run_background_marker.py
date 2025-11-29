db43b51c5e5b75066e2d71285cad6fc96d640ac3
HarryLin46~ <r12942083@ntu.edu.tw>
2025-11-29 13:44:41 +0800

    add run py at root dir

---
+++ run_background_marker.py
@@ -0,0 +1,4 @@
+from background_marker.marker import main
+
+if __name__ == "__main__":
+    main()
---
+++ run_generate_clips.py
@@ -0,0 +1,4 @@
+# from clip_generator.generate_clips import main
+
+if __name__ == "__main__":
+    main()
